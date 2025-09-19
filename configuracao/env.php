087138796afdc5e09828a2360d0e807579ccef44
pc02fabiano <120389156+git-ferpall@users.noreply.github.com>
2025-09-19 11:51:09 -0300

    Update env.php

--- configuracao/env.php
+++ configuracao/env.php
@@ -1,9 +1,6 @@
 <?php
 define('AUTH_API_LOGIN', 'https://frutag.com.br/sso/login.php');
-
-define('JWT_ALGO', 'HS256');                         // <— HS256
-define('JWT_SECRET', '}^BNS8~o80?RyV]d'); // IGUAL ao do Frutag
-// (mantenha JWT_PUBLIC_KEY_PATH definido, mas ele será ignorado no HS256)
+define('JWT_ALGO', 'HS256');
+define('JWT_SECRET', '}^BNS8~o80?RyV]d');  // igual ao SSO
 define('AUTH_COOKIE', 'token');
-define('COOKIE_DOMAIN', '.frutag.com.br');
 
