213caf5796ebd2c602413f7de0427cc0e2a91f71
pc02fabiano <120389156+git-ferpall@users.noreply.github.com>
2025-09-22 16:40:42 -0300

    Update userinfo.php

--- sso/userinfo.php
+++ sso/userinfo.php
@@ -64,13 +64,17 @@ $extra = [];
 try {
     if ($tipo === 'cliente') {
     $st = $pdo->prepare("
-        SELECT cli_empresa, cli_razao_social, cli_cnpj_cpf
+        SELECT 
+            cli_empresa,
+            cli_razao_social,
+            cli_cnpj_cpf
         FROM cliente
         WHERE cli_cod = :id
         LIMIT 1
     ");
     $st->execute([':id' => $id]);
     $extra = $st->fetch(PDO::FETCH_ASSOC) ?: [];
+
 
     } elseif ($tipo === 'usuario') {
         $st = $pdo->prepare("
