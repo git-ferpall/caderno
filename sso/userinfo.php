e9bc42c9a2d753439482dfc45cc955c0f83ba6cd
pc02fabiano <120389156+git-ferpall@users.noreply.github.com>
2025-09-22 16:57:34 -0300

    Update userinfo.php

--- sso/userinfo.php
+++ sso/userinfo.php
@@ -1,7 +1,7 @@
 <?php
-// public_html/sso/userinfo.php
-// Retorna as informações do JWT, sem acessar banco
-// PHP 7.3+
+error_reporting(E_ALL);
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/userinfo_error.log');
 
 @ini_set('display_errors', '0');
 header('Content-Type: application/json; charset=utf-8');
