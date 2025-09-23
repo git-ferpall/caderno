3087f1b22b621ea954253ca890773329af7b87e1
pc02fabiano <120389156+git-ferpall@users.noreply.github.com>
2025-09-23 12:54:03 -0300

    Update propriedade.php

--- home/propriedade.php
+++ home/propriedade.php
@@ -1,21 +1,7 @@
 <?php
 require_once __DIR__ . '/../configuracao/protect.php';
-require_once __DIR__ . '/../configuracao/configuracao_conexao.php';
-require_once __DIR__ . '/../sso/verify_jwt.php';
 
-$payload = verify_jwt();
-$user_id = $payload['sub'] ?? null;
-
-$propriedades = [];
-if ($user_id) {
-    $stmt = $mysqli->prepare("SELECT * FROM propriedades WHERE user_id = ?");
-    $stmt->bind_param("i", $user_id);
-    $stmt->execute();
-    $res = $stmt->get_result();
-    $propriedades = $res->fetch_all(MYSQLI_ASSOC);
-}
 ?>
-
 
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -67,7 +53,7 @@ if ($user_id) {
                         $propriedade = $propriedades[0]; 
                         echo '
                             <div class="item item-propriedade v2" id="prop-' . $propriedade['id'] . '">
-                                <h4 class="item-title">' . $propriedade['nome_razao'] . '</h4>
+                                <h4 class="item-title">' . $propriedade['nome'] . '</h4>
                                 <div class="item-edit">
                                     <button class="edit-btn" id="edit-propriedade" type="button" onclick="altProp()">
                                         Alterar
@@ -160,6 +146,5 @@ if ($user_id) {
     </div>
         
     <?php include '../include/footer.php' ?>
-    <script src="/js/propriedades.js"></script>
 </body>
 </html>
