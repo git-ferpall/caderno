caad8bfdfc748bf669ed4b115339a697fac92e38
pc02fabiano <120389156+git-ferpall@users.noreply.github.com>
2025-09-26 12:43:36 -0300

    Update produtos.php

--- home/produtos.php
+++ home/produtos.php
@@ -55,7 +55,7 @@ require_once __DIR__ . '/../configuracao/protect.php';
 
                                         <!-- Botão Excluir -->
                                         <button class="edit-btn fundo-vermelho" type="button" onclick="deleteProduto(' . $produto['id'] . ')">
-                                            <img src="../img/icon/icon-trash.svg" alt="Excluir" class="icon-trash">
+                                            <div class="edit-icon icon-trash"></div>
                                         </button>
                                     </div>
                                 </div>
