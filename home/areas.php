888d351ccdde0270c6aefbb9d9bb99f93296a50c
pc02fabiano <120389156+git-ferpall@users.noreply.github.com>
2025-09-29 12:40:09 -0300

    Update areas.php

--- home/areas.php
+++ home/areas.php
@@ -113,7 +113,7 @@ require_once __DIR__ . '/../configuracao/protect.php';
 
         <?php include '../include/imports.php' ?>
     </div>
-        
+    <script src="../js/areas.js"></script>    
     <?php include '../include/footer.php' ?>
 </body>
 </html>
