7be577747ebe4a456e31397868184ffc7ef59d28
pc02fabiano <120389156+git-ferpall@users.noreply.github.com>
2025-10-01 15:59:02 -0300

    Update Plantio.php

--- home/Plantio.php
+++ home/Plantio.php
@@ -29,34 +29,33 @@ require_once __DIR__ . '/../configuracao/protect.php';
             <input type="date" id="data" name="data" class="form-text" required>
           </div>
 
-          <!-- ÁREAS -->
           <div class="form-campo">
             <label for="area">Áreas cultivadas</label>
-            <div id="lista-areas">
               <div class="form-box form-box-area">
-                <select name="area[]" class="form-select form-text area-select" required>
+                <select id="area" name="area[]" class="form-select form-text area-select" required>
                   <option value="">Selecione a área</option>
                 </select>
-                <button class="remove-btn remove-area" type="button">x</button>
-              </div>
-            </div>
-            <button class="add-btn add-area" type="button">+</button>
-          </div>
-
-          <!-- PRODUTOS -->
-          <div class="form-campo">
-            <label for="produto">Produtos cultivados</label>
-            <div id="lista-produtos">
-              <div class="form-box form-box-produto">
-                <select name="produto[]" class="form-select form-text produto-select" required>
-                  <option value="">Selecione o produto</option>
+                <button class="add-btn add-area" type="button">
+                  <div class="btn-icon icon-plus cor-branco"></div>
+                </button>
                 </select>
-                <button class="remove-btn remove-produto" type="button">x</button>
+                  <button class="remove-btn remove-area" type="button">x</button>
+                </div>
               </div>
-            </div>
-            <button class="add-btn add-produto" type="button">+</button>
           </div>
 
+
+          <div class="form-campo">
+            <label for="produto">Produto cultivado</label>
+            <div class="form-box form-box-produto">
+              <select id="produto" name="produto" class="form-select form-text" required>
+                <option value="">Selecione o produto</option>
+              </select>
+              <button class="add-btn add-produto" type="button">
+                <div class="btn-icon icon-plus cor-branco"></div>
+              </button>
+            </div>
+          </div>
 
           <div class="form-campo">
             <label for="quantidade">Quantidade</label>
