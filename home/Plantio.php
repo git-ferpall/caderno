24726291e838c977eaf6124368eb5193e6d27e3e
pc02fabiano <120389156+git-ferpall@users.noreply.github.com>
2025-10-01 14:43:18 -0300

    .

--- home/Plantio.php
+++ home/Plantio.php
@@ -30,16 +30,19 @@ require_once __DIR__ . '/../configuracao/protect.php';
           </div>
 
           <div class="form-campo">
-            <label for="area">Área cultivada</label>
-            <div class="form-box form-box-area">
-              <select id="area" name="area[]" class="form-select form-text" multiple required>
-                <option value="">Selecione a área</option>
-              </select>
-              <button class="add-btn add-area" type="button">
-                <div class="btn-icon icon-plus cor-branco"></div>
-              </button>
+            <label for="area">Áreas cultivadas</label>
+            <div id="lista-areas">
+              <div class="form-box form-box-area">
+                <select name="area[]" class="form-select form-text area-select" required>
+                  <option value="">Selecione a área</option>
+                </select>
+              </div>
             </div>
+            <button class="add-btn add-area" type="button">
+              <div class="btn-icon icon-plus cor-branco"></div>
+            </button>
           </div>
+
 
           <div class="form-campo">
             <label for="produto">Produto cultivado</label>
