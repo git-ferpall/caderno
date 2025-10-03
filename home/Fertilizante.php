e5d02e85585c2c7d76f0c9c90c15cecf5ed948f6
pc02fabiano <120389156+git-ferpall@users.noreply.github.com>
2025-10-03 14:34:08 -0300

    Update Fertilizante.php

--- home/Fertilizante.php
+++ home/Fertilizante.php
@@ -43,19 +43,22 @@ require_once __DIR__ . '/../configuracao/protect.php';
           <input type="date" id="data" name="data" class="form-text" required>
         </div>
 
-        <div class="form-campo">
-          <label for="area">Áreas cultivadas</label>
-          <div id="lista-areas">
-            <div class="form-box form-box-area">
-              <select name="area[]" class="form-select form-text area-select" required>
-                <option value="">Selecione a área</option>
-              </select>
+        <!-- ÁREAS -->
+          <div class="form-campo">
+            <label>Áreas cultivadas</label>
+            <div class="linha">
+              <div id="lista-areas" class="lista-areas">
+                <div class="form-box form-box-area">
+                  <select name="area[]" class="form-select form-text area-select" required>
+                    <option value="">Selecione a área</option>
+                  </select>
+                </div>
+              </div>
+              <button class="add-btn add-area" type="button">
+                <div class="btn-icon icon-plus cor-branco"></div>
+              </button>
             </div>
           </div>
-          <button class="add-btn add-area" type="button">
-            <div class="btn-icon icon-plus cor-branco"></div>
-          </button>
-        </div>
 
 
         <div class="form-campo">
