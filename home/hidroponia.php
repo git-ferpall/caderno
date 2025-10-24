dc9d2fd3529887e5b86ef113eb96b5f7d732581e
pc02fabiano <120389156+git-ferpall@users.noreply.github.com>
2025-10-24 13:01:47 -0300

    Update hidroponia.php

--- home/hidroponia.php
+++ home/hidroponia.php
@@ -403,8 +403,9 @@ require_once __DIR__ . '/../configuracao/protect.php';
         <?php include '../include/imports.php' ?>
     </div>
     <script src="../js/hidroponia/estufa.js"></script>
-    <script src="../js/bancada.js"></script>
-    <script src="../js/hidroponia.js"></script> 
+    <script src="../js/hidroponia/bancada.js"></script>
+    <script src="../js/hidroponia/index.js"></script>
+
     <?php include '../include/footer.php' ?>
 </body>
 </html>
