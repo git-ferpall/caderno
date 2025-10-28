9749f23fde2558f5f54db229b301f7b6320b2b8f
pc02fabiano <120389156+git-ferpall@users.noreply.github.com>
2025-10-28 12:23:23 -0300

    Update silo.php

--- home/silo.php
+++ home/silo.php
@@ -36,7 +36,7 @@ require_once __DIR__ . '/../configuracao/protect.php';
 
         <main id="silo" class="sistema">
   <div class="page-title">
-      <h2 class="main-title cor-branco"></h2>
+      <h2 class="main-title cor-branco">SILO DE DADOS</h2>
   </div>
 
     <main id="silo" class="sistema">
