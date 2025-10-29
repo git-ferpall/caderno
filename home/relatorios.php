4e9ef873171f3ba277bda33c4a29f779541198d1
pc02fabiano <120389156+git-ferpall@users.noreply.github.com>
2025-10-29 15:34:46 -0300

    Update relatorios.php

--- home/relatorios.php
+++ home/relatorios.php
@@ -136,6 +136,19 @@ require_once __DIR__ . '/../configuracao/protect.php';
         <?php include '../include/imports.php' ?>
         <script src="../js/relatorios.js"></script>
     </div>
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
+    <script>
+    document.addEventListener("DOMContentLoaded", () => {
+    $('#pf-propriedades').select2({
+        placeholder: "Selecione uma ou mais propriedades",
+        width: '100%',
+        language: "pt-BR"
+    });
+    });
+    </script>
+
         
     <?php include '../include/footer.php' ?>
 </body>
