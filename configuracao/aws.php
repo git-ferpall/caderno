d90144b7750bb735d752eeb813fa2536d60d5d03
pc02fabiano <120389156+git-ferpall@users.noreply.github.com>
2025-12-10 15:30:34 -0300

    .

---
+++ configuracao/aws.php
@@ -0,0 +1,3 @@
+<?php
+define('AWS_KEY', 'SUA_AWS_ACCESS_KEY');
+define('AWS_SECRET', 'SUA_AWS_SECRET_KEY');
--- funcoes/sms/test_sms.php
+++ funcoes/sms/test_sms.php
@@ -1,5 +1,7 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../../vendor/autoload.php';
+require __DIR__ . '/../../configuracao/aws.php';
+
 
 use Aws\Sns\SnsClient;
 
