96fb07dbbe10490ad8761cf065b652e5743b9bfc
pc02fabiano <120389156+git-ferpall@users.noreply.github.com>
2026-01-06 16:38:44 -0300

    Update aws.php

--- configuracao/aws.php
+++ configuracao/aws.php
@@ -1,3 +1,3 @@
 <?php
-define('AWS_KEY', 'AKIAVBMYWQOFYPRQMLM5');
-define('AWS_SECRET', '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');
+//define('AWS_KEY', 'AKIAVBMYWQOFYPRQMLM5');
+//define('AWS_SECRET', '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');
