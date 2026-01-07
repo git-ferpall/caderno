48904c6d13ee95156ef6f31a814c7277fba56538
pc02fabiano <120389156+git-ferpall@users.noreply.github.com>
2026-01-07 16:24:24 -0300

    Update relatorio.php

--- funcoes/email/relatorio.php
+++ funcoes/email/relatorio.php
@@ -3,14 +3,13 @@
  * Relatório semanal de apontamentos
  * Responsável apenas pela geração e envio do e-mail
  */
-
+require_once __DIR__ . '/../../configuracao/configuracao_conexao.php';
+require_once dirname(__DIR__, 2) . '/vendor/autoload.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require_once __DIR__ . '/../../configuracao/configuracao_conexao.php';
-require_once __DIR__ . '/../../libs/PHPMailer/src/PHPMailer.php';
-require_once __DIR__ . '/../../libs/PHPMailer/src/SMTP.php';
-require_once __DIR__ . '/../../libs/PHPMailer/src/Exception.php';
+
+
 
 /* =========================================================
    CONFIGURAÇÕES
