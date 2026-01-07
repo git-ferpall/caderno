75aff71f06ba5a546c536587198c1e803a3e52f5
pc02fabiano <120389156+git-ferpall@users.noreply.github.com>
2026-01-07 12:15:55 -0300

    .

---
+++ funcoes/sms/test_sms2.php
@@ -0,0 +1,33 @@
+<?php
+require __DIR__ . '/../../vendor/autoload.php';
+require __DIR__ . '/../../configuracao/aws.php';
+
+use Aws\Sns\SnsClient;
+
+$sns = new SnsClient([
+    'region' => AWS_REGION,
+    'version' => 'latest',
+    'credentials' => [
+        'key'    => AWS_KEY,
+        'secret' => AWS_SECRET
+    ]
+]);
+
+try {
+    $result = $sns->publish([
+        'Message' => "🔔 Teste Frutag via sistema!\nSeu SMS via PHP está funcionando.",
+        'PhoneNumber' => '+554999346368',
+        'MessageAttributes' => [
+            'AWS.SNS.SMS.SMSType' => [
+                'DataType' => 'String',
+                'StringValue' => 'Transactional'
+            ]
+        ]
+    ]);
+
+    echo "SMS enviado com sucesso!\n";
+    echo "Message ID: " . $result['MessageId'] . "\n";
+
+} catch (Exception $e) {
+    echo "Erro ao enviar SMS: " . $e->getMessage();
+}
