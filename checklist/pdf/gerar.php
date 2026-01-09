49c6397d2268610843e73fa85bac82b5597911cc
pc02fabiano <120389156+git-ferpall@users.noreply.github.com>
2026-01-09 17:05:34 -0300

    .

--- checklist/pdf/gerar.php
+++ checklist/pdf/gerar.php
@@ -1,5 +1,6 @@
 <?php
-require_once __DIR__ . '/../configuracao/configuracao_conexao.php';
+require_once __DIR__ . '/../../configuracao/configuracao_conexao.php';
+
 require_once '../../vendor/autoload.php';
 require_once '../qr.php';
 
