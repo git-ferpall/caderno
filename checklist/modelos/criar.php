a606b9f12eece532ace9ef7c19b89d8792051f60
FABIANO RODRIGUES AMARO <fabianoamaro@pge.sc.gov.br>
2026-01-12 16:49:38 -0300

    Update criar.php

--- checklist/modelos/criar.php
+++ checklist/modelos/criar.php
@@ -266,7 +266,13 @@ document.addEventListener('change', function (e) {
     }
 });
 </script>
-<?php include '/../../../include/imports.php' ?>
-<?php include '/../../../include/footer.php' ?>
+<script src="https://cdn.jsdelivr.net/npm/intl-tel-input@18.1.1/build/js/intlTelInput.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/intl-tel-input@18.1.1/build/js/utils.js"></script>
+
+<!-- JS base do sistema (caminhos absolutos) -->
+<script src="/js/jquery.js"></script>
+<script src="/js/script.js"></script>
+<script src="/js/main.js"></script>
+<script src="/js/popups.js"></script>
 </body>
 </html>
