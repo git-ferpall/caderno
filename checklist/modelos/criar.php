ba23d5a87c577913fe7c96c7a878ccc7d05380af
FABIANO RODRIGUES AMARO <fabianoamaro@pge.sc.gov.br>
2026-01-12 16:51:11 -0300

    Update criar.php

--- checklist/modelos/criar.php
+++ checklist/modelos/criar.php
@@ -60,6 +60,7 @@ if ($id) {
 <head>
     <meta charset="utf-8">
     <title><?= $id ? 'Editar' : 'Criar' ?> modelo de checklist</title>
+    <base href="/">
 
     <!-- Bootstrap + Sortable -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
