be41a87a0081a4ad487d954968594589421b5295
FABIANO RODRIGUES AMARO <fabianoamaro@pge.sc.gov.br>
2026-01-12 17:31:49 -0300

    Update criar.php

--- checklist/modelos/criar.php
+++ checklist/modelos/criar.php
@@ -66,6 +66,8 @@ if ($id) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/sortablejs@1.15.0/Sortable.min.js"></script>
 
+    <!-- CSS do sistema -->
+    <link rel="stylesheet" href="/css/style.css">
 
     <style>
         .handle { cursor: grab }
