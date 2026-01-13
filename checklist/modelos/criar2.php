5e498308aa9d50ff9521d771bb72d78b3c5c7858
FABIANO RODRIGUES AMARO <fabianoamaro@pge.sc.gov.br>
2026-01-13 11:40:54 -0300

    Update criar2.php

--- checklist/modelos/criar2.php
+++ checklist/modelos/criar2.php
@@ -60,6 +60,7 @@ if ($id) {
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<base href="/">
 
 <title><?= $id ? 'Editar' : 'Criar' ?> Modelo de Checklist</title>
 
