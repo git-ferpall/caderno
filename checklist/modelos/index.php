fd043ce4b2e874bbf5351b712c3b86dc09b5cc04
FABIANO RODRIGUES AMARO <fabianoamaro@pge.sc.gov.br>
2026-01-13 16:19:27 -0300

    Update index.php

--- checklist/modelos/index.php
+++ checklist/modelos/index.php
@@ -42,6 +42,7 @@ $stmt->close();
     <base href="/">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="icon" type="image/png" href="/img/logo-icon.png">
+    <link rel="stylesheet" href="/css/style.css">
 </head>
 
 <body class="bg-light">
