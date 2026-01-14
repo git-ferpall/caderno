c945bdd62be7793f31a400af05240bc1861700fa
FABIANO RODRIGUES AMARO <fabianoamaro@pge.sc.gov.br>
2026-01-14 11:27:20 -0300

    Update criar.php

--- checklist/modelos/criar.php
+++ checklist/modelos/criar.php
@@ -58,7 +58,6 @@ if ($id) {
 <head>
     <meta charset="utf-8">
     <title><?= $id ? 'Editar' : 'Criar' ?> modelo</title>
-    <base href="/">
     <link rel="icon" type="image/png" href="/img/logo-icon.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/sortablejs@1.15.0/Sortable.min.js"></script>
