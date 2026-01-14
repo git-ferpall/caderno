82c49edf2fa639ae60ee07a53def61c3a8cb2bab
FABIANO RODRIGUES AMARO <fabianoamaro@pge.sc.gov.br>
2026-01-14 11:33:22 -0300

    Update imports.php

--- checklist/include/imports.php
+++ checklist/include/imports.php
@@ -4,4 +4,4 @@
 <script src="../js/jquery.js"></script>
 <script src="../js/main.js"></script>
 <script src="../js/popups.js"></script>
-<script src="../js/script.js"></script>
+<!--<script src="../js/script.js"></script>-->
