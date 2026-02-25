53838bfa22873eb8c937ebe3615615694145ad43
FABIANO RODRIGUES AMARO <fabianoamaro@pge.sc.gov.br>
2026-02-25 16:06:50 -0300

    Update salvar_bancada.php

--- funcoes/salvar_bancada.php
+++ funcoes/salvar_bancada.php
@@ -1,6 +1,4 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 
 require_once __DIR__ . '/../configuracao/configuracao_conexao.php';
 require_once __DIR__ . '/../sso/verify_jwt.php';
