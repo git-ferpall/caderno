fd433c2de97a3ad4a18f4ab748bfbca5fe0de23d
FABIANO RODRIGUES AMARO <fabianoamaro@pge.sc.gov.br>
2026-02-27 16:08:49 -0300

    Update relatorios2.php

--- home/relatorios2.php
+++ home/relatorios2.php
@@ -85,7 +85,7 @@ require_once __DIR__ . '/../configuracao/protect.php';
             </main>
 
         <?php include '../include/imports.php' ?>
-        <script src="../js/relatorios.js"></script>
+        
     </div>
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
