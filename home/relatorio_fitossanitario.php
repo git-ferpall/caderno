05f1bafe88c5c04c287b8633796aac4ec3d16cfb
FABIANO RODRIGUES AMARO <fabianoamaro@pge.sc.gov.br>
2026-03-19 16:07:50 -0300

    Update relatorio_fitossanitario.php

--- home/relatorio_fitossanitario.php
+++ home/relatorio_fitossanitario.php
@@ -36,7 +36,9 @@ require_once __DIR__ . '/../configuracao/protect.php';
 <div id="loading" style="display:none;"></div>
 <?php include '../include/popups.php'; ?> 
 <?php include '../include/menu.php'; ?>
-
+<div id="loading" style="display:none;">
+  <img id="loading-img" src="" style="display:none;">
+</div>
 <div id="pdf-loading" style="display:none;position:fixed;inset:0;background:rgba(255,255,255,0.8);z-index:9999;align-items:center;justify-content:center;">
     <div style="text-align:center">
         <div class="spinner"></div>
