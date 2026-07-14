72a9d4ec769bcd2088664c849f703c0313f44a25
FABIANO RODRIGUES AMARO <fabianoamaro@pge.sc.gov.br>
2026-07-14 15:00:21 -0300

    Update recaptcha.php

--- configuracao/recaptcha.php
+++ configuracao/recaptcha.php
@@ -2,6 +2,8 @@
 // configuracao/recaptcha.php
 require_once __DIR__ . '/secrets_loader.php';
 
-define('RECAPTCHA_SITE_KEY', caderno_secret('RECAPTCHA_SITE_KEY', ''));
+// A site key é pública (aparece no HTML da página), então pode ficar no código.
+// A secret key é confidencial: só via variável de ambiente ou secrets.php.
+define('RECAPTCHA_SITE_KEY', caderno_secret('RECAPTCHA_SITE_KEY', '6LcB3OIrAAAAAKTdgcdNzRMV63Wd3QNQ47DCvFEH'));
 define('RECAPTCHA_SECRET_KEY', caderno_secret('RECAPTCHA_SECRET_KEY', ''));
 ?>
